62453417f858f16f336d783480e0760091078ff0
Varol Aksoy <varolaksoy@gmail.com>
2022-01-28 21:42:56 +0300

    add network manager to core api

--- FreeNowCoreAPI/Sources/FreeNowCoreAPI/FreeNowCoreAPI.swift
+++
@@ -1,6 +0,0 @@
-public struct FreeNowCoreAPI {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- FreeNowCoreAPI/Tests/FreeNowCoreAPITests/FreeNowCoreAPITests.swift
+++ FreeNowCoreAPI/Tests/FreeNowCoreAPITests/FreeNowCoreAPITests.swift
@@ -6,6 +6,5 @@ final class FreeNowCoreAPITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(FreeNowCoreAPI().text, "Hello, World!")
     }
 }
